d96b720001bd9506f3b3cedf22686a58ae2d2b42
Yash <youremail@domain.com>
2023-04-24 15:24:16 -0400

    Final Changes

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -1,5 +1,5 @@
 import unittest
-#from translator import english_to_french, french_to_english
+from .translator import english_to_french, french_to_english
 
 class TestMachineTranslation(unittest.TestCase):
 
